ee11368e35f51a5622f7fc8d3633603cdecde631
SrWoonejos <danmirandam@gmail.com>
2024-09-30 18:08:08 -0300

    update

---
+++ main.py
@@ -0,0 +1,25 @@
+#PRUEBA M3 - INTRODUCCION A PYTHON3
+
+#lista nombres
+nombres = ['Harry Houdini', 'Newton', 'David Blaine', 'Hawking', 'Messi', 'Teller', 'Einstein', 'Pele', 'Juanes']
+
+#funcion para magos
+def hacer_grandioso(magos):
+    for i in ran(len(magos)):
+        magos[i] = 'El gran ' + magos[i]
+
+#funcion nombres lista
+def print_nombres(lista):
+    for nombre in lista:
+        print(nombre)
+
+#separar nombres en categorias
+def nombre_grupos(nombres):
+    magos = []
+    cientificos = []
+    otros = []
+
+for nombre in nombres:
+    if nombre in ['Harry Houdini', 'David Blaine', 'Teller']:
+        magos.append(nombre)
+    elif nombre in     
